8fa6ce338ba619b8fddd38bc0c9f15dc61ef9e0a
Awais-Khan-G <asjd8037@gmail.com>
2024-10-30 11:12:05 +0500

    Update

--- Ticket Booking System/ticket.py
+++ Ticket Booking System/ticket.py
@@ -1,51 +1,83 @@
-from_location_data = {
-    
-    "1": "Los Angeles",
-    "2": "Chicago",
-    "3": "San Francisco",
-    "4": "Miami",
-    "5": "Seattle",
-    "6": "Boston",
-    "7": "Houston",
-    "8": "Dallas",
-    "9": "Atlanta",
-    "10": "Philadelphia"
+days = {
+    "Monday": "Monday",
+    "Tuesday": "Tuesday",
+    "Wednesday": "Wednesday",
+    "Thursday": "Thursday",
+    "Friday": "Friday"
 }
 
-to_location_data = {
-    
-    "1": "Pakistan",
-    "2": "America",
-    "3": "UAE",
-    "4": "India",
-    "5": "Bangladesh",
-    "6": "Quwait",
-    "7": "Saudia Arab",
-    "8": "Malaysia",
-    "9": "Iran",
-    "10": "Phalestine"
+ticket_amount = {
+    "Monday": 1000,
+    "Tuesday": 2000,
+    "Wednesday": 3000,
+    "Thursday": 4000,
+    "Friday": 5000,
 }
 
-
-
+amount = 0
 
 print("Hello Welcome to the \033[1mTECH-AIRLINES\033[0m")
 
-# from_location : str = input("Enter you location")  
-# to_location : str = input("Enter you location") 
+print("""
+    FROM Location
 
-print(f"""
-    "1": "Los Angeles",
-    "2": "Chicago",
-    "3": "San Francisco",
-    "4": "Miami",
-    "5": "Seattle",
-    "6": "Boston",
-    "7": "Houston",
-    "8": "Dallas",
-    "9": "Atlanta",
-    "10": "Philadelphia"
+     Los Angeles
+     Chicago
+     San Francisco
+     Miami
+     Seattle
+     Boston
+     Houston
+     Dallas
+     Atlanta
+     Philadelphia
+
 """)
 
-from_location = input("Enter Your Location")
-to_location = input("Enter Your Location")
+from_location = input("Enter Your Location: ")
+
+print("""
+    To Location
+
+    Pakistan
+    America
+    UAE
+    India
+    Bangladesh
+    Quwait
+    Saudia Arab
+    Malaysia
+    Iran
+    Phalestine
+
+""")
+
+to_location = input("Please Enter Your Destination Location: ")
+
+print("""
+    Monday    : 1
+    Tuesday   : 2
+    Wednesday : 3
+    Thursday  : 4
+    Friday    : 5
+""")
+
+select_days = input("Enter day: ")
+
+if select_days in days:
+    day_name = days[select_days]
+
+    if day_name in ticket_amount:
+      
+        amount += ticket_amount[day_name]
+        print("You have selected", day_name)
+else:
+    print("Invalid day")
+
+print(f"""
+Congratulations! Your plane ticket has been successfully booked. Safe travels!
+From Location : {from_location}
+To Location : {to_location}
+Days : {day_name}
+Amount : {amount}
+""")
